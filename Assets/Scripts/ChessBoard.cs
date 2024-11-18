e5195eb49c72d0f188542671b67c23cdf60ce554
Steven Lim <limstevenlbw@gmail.com>
2024-11-17 23:58:11 -0800

    piece adjustment

---
+++ Assets/Scripts/ChessBoard.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessBoard : MonoBehaviour
+{
+    //Bitboards
+    long blk_Pawns, blk_Knights, blk_Bishops, blk_Rooks, blk_King, blk_Queens;
+    long wht_Pawns, wht_Knights, wht_Bishops, wht_Rooks, wht_King, wht_Queens;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
