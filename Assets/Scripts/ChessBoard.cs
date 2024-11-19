37f91c12c1565ff16c6e48dfb13a2e48733c83c3
Steven Lim <limstevenlbw@gmail.com>
2024-11-18 23:56:11 -0800

    FEN to Bitboard Progress

--- Assets/Scripts/ChessBoard.cs
+++ Assets/Scripts/ChessBoard.cs
@@ -8,6 +8,16 @@ public class ChessBoard : MonoBehaviour
     long blk_Pawns, blk_Knights, blk_Bishops, blk_Rooks, blk_King, blk_Queens;
     long wht_Pawns, wht_Knights, wht_Bishops, wht_Rooks, wht_King, wht_Queens;
 
+    //"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    public void FENtoBitBoard(string FEN)
+    {
+        char[] fenArr = FEN.ToCharArray();
+        for(int i = 0; i <= fenArr.Length; i++)
+        {
+
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/InputFEN.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InputFEN : MonoBehaviour
+{
+    [SerializeField] private ChessBoard board;
+    
+    private TMP_InputField input;
+    private bool isFocused;
+    private string initialFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input = GetComponent<TMP_InputField>();
+        input.onSelect.AddListener((string text) => isFocused = true);
+        input.onDeselect.AddListener((string text) => isFocused = false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Note, do not use input.isFocused, that becomes false when Return is pressed before the if-statement can run
+        if (isFocused && Input.GetKeyDown(KeyCode.Return)) board.FENtoBitBoard(input.text);
+    }
+}
