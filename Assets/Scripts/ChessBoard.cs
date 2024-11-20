f9a3130dd0a07cb56f5e73ef2de646b8b3d98b55
Steven Lim <limstevenlbw@gmail.com>
2024-11-19 22:14:33 -0800

    FEN should direct to chessboard instead of bitboard

--- Assets/Scripts/ChessBoard.cs
+++ Assets/Scripts/ChessBoard.cs
@@ -5,8 +5,53 @@ using UnityEngine;
 public class ChessBoard : MonoBehaviour
 {
     //Bitboards
-    long blk_Pawns, blk_Knights, blk_Bishops, blk_Rooks, blk_King, blk_Queens;
-    long wht_Pawns, wht_Knights, wht_Bishops, wht_Rooks, wht_King, wht_Queens;
+    ulong blk_Pawns, blk_Knights, blk_Bishops, blk_Rooks, blk_King, blk_Queens;
+    ulong wht_Pawns, wht_Knights, wht_Bishops, wht_Rooks, wht_King, wht_Queens;
+
+    [SerializeField] private List<ChessBoardFile> file = new List<ChessBoardFile>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // files[0] = a_File; files[1] = b_File; files[2] = c_File; files[3] = d_File;
+        //files[4] = e_File; files[5] = f_File; files[6] = g_File; files[7] = h_File;
+    }
+
+    //Starting FEN
+    //"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    public void FENtoChessBoard(string FEN)
+    {
+        char[] fenArr = FEN.ToCharArray();
+        int i = 0;
+        int skip = 0;
+
+        for (int row = 7; row >= 0; row--)
+        {
+            for(int col = 0; col < 8; col++)
+            {
+                if (skip > 0)
+                {
+                    skip--;
+                    continue;
+                }
+
+                char c = fenArr[i];
+                i++;
+                if (char.IsNumber(c)) skip = c - 0;
+
+                file[col].CreatePiece(row, c);
+            }
+        }
+
+        /*
+        for(int i=0; i < fenArr.Length; i++)
+        {
+            char c = fenArr[i];
+
+            if(char.IsNumber(c)
+        }
+        */
+    }
 
     //"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     public void FENtoBitBoard(string FEN)
@@ -18,7 +63,7 @@ public class ChessBoard : MonoBehaviour
         int j = 0; //FEN pointer
         int skip = 0;
 
-        for (int i = 63; i <= 0; i--)
+        for (int i = 0; i < 1; i++)
         {
             if(skip > 0)
             {
@@ -32,19 +77,20 @@ public class ChessBoard : MonoBehaviour
             if (char.IsNumber(c)) skip = c - 0;
 
             string b = "0000000000000000000000000000000000000000000000000000000000000000";
+            b = b.Substring(0, i) + "1" + b.Substring(i+1, b.Length - 1);
 
-
-
+            switch (c)
+            {
+                case ('r'): blk_Rooks += ulong.Parse(b);
+                    break;
+            }
+                
 
 
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/ChessBoardFile.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessBoardFile : MonoBehaviour
+{
+    public void CreatePiece(int row, char piece)
+    {
+        switch (piece)
+        {
+            case ('r'):
+                break;
+            case ('n'):
+                break;
+            case ('b'):
+                break;
+            case ('k'):
+                break;
+            case ('q'):
+                break;
+            case ('p'):
+                break;
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
